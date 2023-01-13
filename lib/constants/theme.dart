285b94a879020c8f1434858dad73b788cba0a06e
aakash-301 <aakashshivanshu5@gmail.com>
2023-01-13 22:58:13 +0530

    App wide theme and textStyle class

---
+++ lib/constants/theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColor{
+  static const Color ingredientsColor = Colors.amber;
+}
+
+class AppTheme {
+  static const TextStyle headingTheme = TextStyle(
+      fontSize: 20, fontFamily: 'RobotoCondensed', fontWeight: FontWeight.bold);
+}
