56abb8309de86bc426d354ce3242f7c868c09ddf
aakash-301 <aakashshivanshu5@gmail.com>
2023-01-14 09:26:55 +0530

    added appLevel routes in constants

--- lib/constants/constant.dart
+++ lib/constants/constant.dart
@@ -2,4 +2,5 @@ class AppRoutes {
   static String home = '/';
   static String categoryMeals = '/category-meals';
   static String mealDetails = '/meal-details';
+  static String filters = '/filters';
 }
